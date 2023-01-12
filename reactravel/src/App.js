0e2e4d8b2a93da41b82e7af0ceec55063b9ddbdc
Lorran de Barros <lorranbarrinha@gmail.com>
2023-01-12 08:48:04 -0300

    Main NavBar frontend style

--- reactravel/src/App.js
+++ reactravel/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import './index.css'
 
 function App() {
   return (
--- reactravel/src/components/navbar/Navbar.js
+++ reactravel/src/components/navbar/Navbar.js
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BiSearch} from 'react-icons/bi'
 import {BsPerson} from 'react-icons/bs'
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaFacebook, FaTwitter, FaYoutube} from 'react-icons/fa'
 
 import './NavbarStyles.css'
+
 
 function NavBar()
 {
     return(
     <div className='navbar'>
-           <div className="logo">
+    
+        <div className="logo">
             <h2>REACTRAVEL</h2>
-            <ul className="nav-menu">
+        </div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>Destinations</li>
+            <li>Travel</li>
+            <li>Booking</li>
+            <li>Views</li>
+        </ul>
+        <div className="nav-menu-icons">
+            <BiSearch className='menu-icon' />'
+            <BsPerson className='menu-icon' />'
+        </div>
+        <div className="full-menu">
+            <HiOutlineMenuAlt4 className='menu-icon' />
+        </div>
+
+        <div className="mobile-menu">
+            <ul className="mobile-navbar">
                 <li>Home</li>
                 <li>Destinations</li>
                 <li>Travel</li>
-                <li>Book</li>
+                <li>Booking</li>
                 <li>Views</li>
             </ul>
-            <div className="nav-icons">
-                <BiSearch className='icon'/>
-                <BsPerson className='icon'/>
-            </div>
-                <div className="hamburguer">
-                    <HiOutlineMenuAlt4 className='icon'/>
+            <div className="mobile-menu-bottom">
+                <div className="mobile-menu-icons">
+                <button>Search</button>
+                <button>My Account</button>
                 </div>
-           </div>
+                <div className="social-media-icons">
+                    <FaFacebook className='menu-icon' />
+                    <FaTwitter className='menu-icon' />
+                    <FaYoutube className='menu-icon' />
+                </div>
+            </div>
+        </div>
+
     </div>
     )
 }
